3e379fd66439cec5739f2b70ecca0423d45ef0de
drkirkp <154014621+drkirkp@users.noreply.github.com>
2024-01-16 14:17:13 -0600

    updated CSS, added more cities

--- index.js
+++ index.js
@@ -22,6 +22,18 @@ function updateTime() {
       "h:mm:ss [<small>]A[</small>]"
     );
   }
+
+  let tokyoElement = document.querySelector("#tokyo");
+  if (tokyoElement) {
+    let tokyoDateElement = tokyoElement.querySelector(".date");
+    let tokyoTimeElement = tokyoElement.querySelector(".time");
+    let tokyoTime = moment().tz("Asia/Tokyo");
+
+    tokyoDateElement.innerHTML = moment().format("MMMM Do YYYY");
+    tokyoTimeElement.innerHTML = tokyoTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 function updateCity(event) {
@@ -42,6 +54,7 @@ function updateCity(event) {
     "A"
   )}</small></div>
   </div>
+  <br>
   <a href="index.html">All Cities</a>
   `;
 }
